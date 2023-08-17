2a2f7f5cf5cc894be63fc7aafc8b10c3e7fc50f1
ItsValkyrie <130717159+ItsValkyrie@users.noreply.github.com>
2023-08-17 12:55:58 -0400

    Agregada Verificación de URLs :link:
    :small_blue_diamond: Agregada función: 'test_within_search_limit'
    :small_blue_diamond: Agregada función: 'test_url_data_indexation'

--- web_scrapper.py
+++ web_scrapper.py
@@ -5,9 +5,6 @@ from bs4 import BeautifulSoup
 # -------------------------------------------------------------------------------------------------
 
 # Funciones:
-def generate_url_list(unidades_academicas:dict, unidades_academicas_extensas:list, url_data):
-    pass
-
 def assemble_url(url_data: dict, campus:str, unidad_academica: int, *modulo) -> str:
     """
     This function returns a string, representing a url for a search query that can be used by 'scrape_courses_data'
@@ -25,6 +22,45 @@ def assemble_url(url_data: dict, campus:str, unidad_academica: int, *modulo) -> 
 
     url += url_data['url_components']['tail']
     return url
+
+def test_within_search_limit(url:str) -> bool:
+    """
+    This function checks if a search query complies with the result limit 'buscacursos'
+    is allowed to show without cropping the results. Returning 'False' if the limit is exceeded, 
+    or 'True' if it isn't
+    """
+    response = requests.get(url)
+    html_content = response.content
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    try:
+        soup.find('div', class_='bordeBonito').text.strip()
+        return False
+
+    except:
+        return True
+
+
+def test_url_data_indexation(unidades_academicas:dict, unidades_academicas_extensas:list, unidades_academicas_sin_datos: list, url_data: dict):
+    todo_correcto = True
+
+    for ua in unidades_academicas:
+
+        if ua not in unidades_academicas_sin_datos:
+
+            if (ua not in unidades_academicas_extensas) == test_within_search_limit(assemble_url( url_data, 'San+Joaquín', unidades_academicas[ua])):
+                pass
+                print(f'{ua} correctamente indexado')
+
+            else:
+                todo_correcto = False
+                print(f'ERROR en {ua}')
+
+    if todo_correcto:
+        print('Dataset Correcto')
+
+                
 
 
 def extract_row_data(row) -> list:
@@ -96,6 +132,86 @@ if __name__ == "__main__":
                                 "tail": "#resultados"
                                 }
         }
+    
+    unidades_academicas_extensas = [
+        "agronomia",
+        "antropologia",
+        "ciencia_politica",
+        "astrofisica",
+        "ciencias_de_la_salud",
+        "construccion_civil",
+        "economia",
+        "educacion",
+        "enfermeria",
+        "filosofia",
+        "fisica",
+        "geografia",
+        "historia",
+        "ingenieria",
+        "letras",
+        "matematicas",
+        "odontologia",
+        "psicologia",
+        "quimica",
+        "requisito_idioma",
+        "sociologia",
+        "teologia",
+        "trabajo_social"
+        ]
 
-    test_url = assemble_url(aaaa, 'San+Joaquín', 0, "W5=W5")
-    print(test_url)
+    unidades_academicas = {
+        "actividades_filosofia": 68,
+        "actividades_universitarias": 0,
+        "actuacion": 34,
+        "agronomia": 11,
+        "antropologia": 92,
+        "arquitectura": 94,
+        "arte": 33,
+        "astrofisica": 2,
+        "bachillerato": 7,
+        "CARA": 52,
+        "ciencia_politica": 45,
+        "ciencias_biologicas": 12,
+        "ciencias_de_la_salud": 16,
+        "college": 9,
+        "comunicaciones": 28,
+        "construccion_civil": 1,
+        "deportes": 53,
+        "derecho": 17,
+        "desarrollo_sustentable": 25,
+        "diseno": 59,
+        "economia": 5,
+        "educacion": 20,
+        "enfermeria": 13,
+        "escuela_de_gobierno": 19,
+        "escuela_de_graduadoos": 40,
+        "estudios_urbanos": 95,
+        "estetica": 51,
+        "filosofia": 67,
+        "fisica": 3,
+        "geografia": 57,
+        "historia": 56,
+        "ing_mat": 23,
+        "ingenieria": 4,
+        "ing_bio_med": 18,
+        "eticas_aplicadas": 26,
+        "letras": 64,
+        "matematicas": 6,
+        "medicina": 14,
+        "veterniaria": 24,
+        "musica": 70,
+        "odontologia": 15,
+        "psicologia": 29,
+        "quimica": 10,
+        "farmacia": 8,
+        "requisito_idioma": 54,
+        "sociologia": 91,
+        "teologia": 38,
+        "trabajo_social": 30,
+        "villarica": 21
+        }
+
+    unidades_sin_datos = ["actividades_filosofia", "arquitectura", "bachillerato", "college", "deportes", "farmacia", "villarica"]
+
+    
+    

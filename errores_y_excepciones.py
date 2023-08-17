88993f3778aa7b6bc05534afbe6953e8b2a942d5
ItsValkyrie <130717159+ItsValkyrie@users.noreply.github.com>
2023-08-17 12:54:26 -0400

    Agregado Errores durante WebScrapping :globe_with_meridians:

--- errores_y_excepciones.py
+++ errores_y_excepciones.py
@@ -4,11 +4,16 @@ mensajes_error = {
     -1000: "\nERROR: el número ingresado no se encuentra dentro de las opciones desplegadas",
     -2000: "\nERROR: el input ingresado no corresponde a un número, " 
             + "ni a la opción Salir del programa",
-    1000: "\nERROR: la lista de búsqueda ingresada se encuentra vacía"
+    1000: "\nERROR: la lista de búsqueda ingresada se encuentra vacía",
+    2000: "\nERROR: la busqueda realiza excede el limite de resultados que puede mostrar 'buscacursos'"
     }
 
 # -------------------------------------------------------------------------------------------------
 
 # Clases y manejo de excepciones:
 class ErrorInputFueraDeRango(Exception):
+    pass
+
+class ErrorBusquedaMuyAmplia(Exception):
+    # error asociado: 2000
     pass
